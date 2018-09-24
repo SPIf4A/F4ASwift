9328284220b856a8556151b96d23d5bfc88e5aa9
Raja <spi@rajas-macbook-air.local>
2018-09-24 08:39:46 +0530

    Initial Commit

---
+++ F4ASwiftDemo/F4A-bridging.h
@@ -0,0 +1,15 @@
+//
+//  F4A-bridging.h
+//  F4ASwiftDemo
+//
+//  Created by Raja on 23/09/2018.
+//  Copyright © 2018 Raja. All rights reserved.
+//
+
+#ifndef F4A_bridging_h
+#define F4A_bridging_h
+
+#import <F4A/FantasyManager.h>
+
+
+#endif /* F4A_bridging_h */
--- F4ASwiftDemo/ViewController.swift
+++ F4ASwiftDemo/ViewController.swift
@@ -8,12 +8,19 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        FantasyManager.fantasyBegin().initialize(withGameID: "5", apiKey: "25d96c538508c648ac7c4e9c89c514f9f55a05b2cbc5b4bcc34bd14e94a05ffb")
+        
+        FantasyManager.fantasyBegin().inView(self)
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
